5b85f6b9759e591716da359046f84cd18c0db225
HongLinh <vohonglinh229@gmail.com>
2015-12-08 02:36:48 +0700

    Update

--- js/myjs.js
+++ js/myjs.js
@@ -1,3 +1,15 @@
-$(document).ready(function(){
 
-});
+
+var find = function() {
+	var url = $('#url').val();
+	$('#content').html('<object id="webloader" data="'+url+'" type="text/html"/>');
+	$( document ).ready(function() {
+		// find the element needed		
+		var t=document.querySelector("#webloader");
+		var htmlDocument= t.contentDocument;
+		// alert to check
+		console.log(htmlDocument);
+	});
+		
+	
+}
